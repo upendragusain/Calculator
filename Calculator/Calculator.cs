00b30adc1fc92fe2748950bf9d3242f5e6d0788c
Upendra Gusain <upendra.gusain@gmail.com>
2020-08-27 10:29:43 +0100

    powers and roots test succeed again

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -18,70 +18,61 @@ namespace Calculator
                 new Dictionary<string, Operation>();
 
             operations.Add("^", new Operation("-?\\d+\\^-?\\d+",
-                Math.Pow));
+                ProcessPowerAndRoot));
 
-            operations.Add("/", new Operation("-?\\d+\\/-?\\d+",
-                (x, y) => { return x / y; }));
+            //operations.Add("/", new Operation("-?\\d+\\/-?\\d+",
+            //    (x, y) => { return x / y; }));
 
-            operations.Add("*", new Operation("-?\\d+\\*-?\\d+",
-                (x, y) => { return x * y; }));
+            //operations.Add("*", new Operation("-?\\d+\\*-?\\d+",
+            //    (x, y) => { return x * y; }));
 
-            operations.Add("+OR-", new Operation("[\\+\\-]?\\d+",
-               (x, y) => { return x + y; }));
+            //operations.Add("+OR-", new Operation("[\\+\\-]?\\d+",
+            //   (x, y) => { return x + y; }));
 
+            string processedInput = string.Empty;
             foreach (var operation in operations)
             {
-                Regex regex = new Regex(operation.Value.Pattern);
-                MatchCollection matches = regex.Matches(input);
-
-                if (operation.Key != "+OR-")
-                {
-                    foreach (Match match in matches)
-                    {
-                        double left = 0d;
-                        double right = 0d;
-                        double operationResult = 0;
-
-                        var numbers = match.Value.Split(operation.Key);
-                        double.TryParse(numbers[0], out left);
-                        double.TryParse(numbers[1], out right);
-                        operationResult = operation.Value.Function(left, right);
-
-                        input = input.Replace(match.Value, operationResult.ToString());
-                    }
-                }
-                else
-                {
-                    double value = 0d;
-                    double operationResult = 0;
-                    foreach (Match match in matches)
-                    {
-                        double.TryParse(match.Value, out value);
-                        operationResult = operationResult + value;
-
-                        input = input.Replace(match.Value, string.Empty);
-                    }
-
-                    input = operationResult.ToString();
-                }
+                processedInput = operation.Value.Process(input, operation.Value.Pattern, operation.Key);
             }
 
             double result;
-            if (double.TryParse(input, out result))
+            if (double.TryParse(processedInput, out result))
                 return result;
 
             return 0;
+        }
+
+        //pow
+        public static string ProcessPowerAndRoot(string input, string pattern, string key)
+        {
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                double left = 0d;
+                double right = 0d;
+                double operationResult = 0d;
+
+                var numbers = match.Value.Split(key);
+                double.TryParse(numbers[0], out left);
+                double.TryParse(numbers[1], out right);
+                operationResult = Math.Pow(left, right);
+
+                input = input.Replace(match.Value, operationResult.ToString());
+            }
+            return input;
         }
     }
 
     public class Operation
     {
         public string Pattern { get; }
-        public Func<double, double, double> Function { get; }
-        public Operation(string pattern, Func<double, double, double> function)
+        public Func<string, string, string, string> Process { get; }
+        public Operation(string pattern, Func<string, string, string, string> function)
         {
             Pattern = pattern;
-            Function = function;
+            Process = function;
         }
     }
 }
